c1b16ac641e0eb380b54559fb07ce32349538882
Jade-glitchh <lawaljadesola943@gmail.com>
2025-06-23 16:32:28 +0100

    I just have to commit

---
+++ for loop.py
@@ -0,0 +1,8 @@
+planets = ['sun', 'mercury', 'venus', 'earth', 'mars']
+for planet in planets:
+    if(planet == 'sun'):
+        print(planet, "is not a planet")
+    else:
+        print(planet, "is a planet")
+    if(planet == 'mercury'):
+        print(planet, "is closest to the sun")   
